e2dff41dc9e4183256cc990ca64d50493640d138
AnasOnGit <50570153+AnasOnGit@users.noreply.github.com>
2026-06-21 11:32:15 +0300

    enh: add telescope and other enhancements, i guess

--- lua/pack.lua
+++ lua/pack.lua
@@ -638,6 +638,10 @@ pcall(require("telescope").load_extension, "ui-select")
 pcall(require("telescope").load_extension, "harpoon")
 
 local builtin = require("telescope.builtin")
+
+-- Find all ref
+vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, { desc = "Find References" })
+
 vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
 vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
 vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
