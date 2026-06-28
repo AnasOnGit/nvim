5c324752382287d95d695bb4b0e622b175f0d76b
AnasOnGit <50570153+AnasOnGit@users.noreply.github.com>
2026-06-28 17:11:46 +0300

    feat: Angular LSP support

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,6 +1,6 @@
 require("mason").setup()
 
-vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = "Go to definition" })
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = "Format Local buffer" })
 vim.keymap.set("n", "df", vim.diagnostic.open_float, { desc = "Show line diagnostics" })
 
@@ -13,17 +13,17 @@ capabilities.textDocument.foldingRange = { dynamicRegistration = false, lineFold
 vim.lsp.config("*", { capabilities = capabilities })
 
 vim.lsp.config("lua_ls", {
-    settings = {
-        Lua = {
-            diagnostics = { globals = { "vim" } },
-        },
-    },
+	settings = {
+		Lua = {
+			diagnostics = { globals = { "vim" } },
+		},
+	},
 })
 
 vim.lsp.enable({
-    "lua_ls",
-    "marksman",
-    "gopls",
-    "rust_analyzer",
+	"lua_ls",
+	"marksman",
+	"gopls",
+	"rust_analyzer",
+	"angularls",
 })
-
